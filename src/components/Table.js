ddf1b3aa5ab1d7ddea365bdf07babee32d6d65ef
Leonardo Elias Souza de Oliverira Meireles <leonardoelias802@gmail.com>
2022-11-03 16:48:54 -0300

    fazendo pedido 7

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
+    const expensesAdptacao = expenses.map((e) => {
+      const moedaSelect = e.currency;
+      const convercao = Number(e.value) * Number(e.exchangeRates[moedaSelect].ask);
+      const cambioDoMomento = Number(e.exchangeRates[moedaSelect].ask);
+      const valorDecimal = Number(e.value);
+      return {
+        description: e.description,
+        tag: e.tag,
+        method: e.method,
+        value: valorDecimal.toFixed(2),
+        currency: e.exchangeRates[moedaSelect].name,
+        cambio: cambioDoMomento.toFixed(2),
+        valorConvertido: convercao.toFixed(2),
+        moedaConvercao: 'Real',
+      };
+    });
     return (
       <>
         <h1>Table</h1>
         <table border="1">
+
           <tr>
             <th>Descrição</th>
             <th>Tag</th>
@@ -17,11 +37,26 @@ class Table extends Component {
             <th>Moeda de conversão</th>
             <th>Editar/Excluir</th>
           </tr>
+
+          <tbody>
+            {expensesAdptacao.map((e, x) => {
+              const valores = Object.values(e);
+              return <tr key={ x }>{valores.map((el, i) => <td key={ i }>{el}</td>)}</tr>;
+            })}
+          </tbody>
+
         </table>
       </>
 
     );
   }
 }
+Table.propTypes = {
+  expenses: PropTypes.arrayOf.isRequired,
+};
 
-export default Table;
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+export default connect(mapStateToProps)(Table);
